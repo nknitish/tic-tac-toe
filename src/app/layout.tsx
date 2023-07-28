c60732809325eab4c752b156f42b1cb28beb327d
Nitish Kumar <nitkumar30@WKMZT49F89C6.global.publicisgroupe.net>
2023-07-28 14:33:39 +0530

    replaced icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.jpg" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
